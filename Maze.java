48463bd76f77a9dbc0b92b2132a914a8af5f2fba
Caitlin Stanton <livelaughlovelol4@yahoo.com>
2015-02-09 08:29:43 -0500

    Update Maze.java

--- Maze.java
+++ Maze.java
@@ -4,4 +4,13 @@ BASE CASE: hit a wall
 
 Don't move to the right--recursively call to go to the right
 Solve(x,y) -- Solve(x+1,y)
+
+Solve(x,y) {
+           Done if B[x][y] = exit
+           Return to previous strp if B[x][y] = wall
+           Otherwise: Solve(x+1,y)
+                      Solve(x-1,y)
+                      Solve(x,y+1)
+                      Solve(x,y-1)
+}
 */
