09a20caee13347cf2fe8ca9b9de62b2835a34e09
Caitlin Stanton <livelaughlovelol4@yahoo.com>
2015-02-26 21:51:18 -0500

    C

--- 04-MergeSort/StuySort.java
+++ 04-MergeSort/StuySort.java
@@ -2,46 +2,62 @@ import java.io.*;
 import java.util.*;
 
 public class StuySort {
-    
-    private ArrayList<Integer> A = new ArrayList<Integer>();
-    private ArrayList<Integer> B = new ArrayList<Integer>();
-
-
-    public StuySort() {
-
-    }
-
-    public ArrayList<Integer> merge(ArrayList<Integer> A, ArrayList<Integer> B) {
-	ArrayList<Integer> result = new ArrayList<Integer>();
-	while (A.size() > 0 && B.size() > 0) {
-	    int counter = 0;
-	    if (A.get(0) <= B.get(0)) {
-		result.add(A.get(0));
-		A.remove(0);
-	    } else {
-		result.add(B.get(0));
-		B.remove(0);
-	    }
-	    counter++;
+	
+    public ArrayList<Integer> mergeSort(ArrayList<Integer> n) {
+	if(n.size() <= 1) {
+	    return n;
 	}
+	ArrayList<Integer> result = new ArrayList<Integer>();
+	
+	ArrayList<Integer> left = new ArrayList<Integer>();
+	ArrayList<Integer> right = new ArrayList<Integer>();
+	int middle = n.size()/2;
+
+	for(int i = 0; i < n.size(); i++) {
+	    if(i < middle) {
+		left.add(n.get(i));
+	    } else {
+		right.add(n.get(i));
+	    }
+	}
+	left = mergeSort(left); 
+	right = mergeSort(right);
+
+        result = merge(left, right);
 	return result;
     }
-
-    public static void main(String[] args) {
-	StuySort c = new StuySort();
-	ArrayList<Integer> A = new ArrayList<Integer>();
-	ArrayList<Integer> B = new ArrayList<Integer>();
-	A.add(2);
-	A.add(5);
-	A.add(9);
-	A.add(14);
-	B.add(6);
-	B.add(13);
-	B.add(19);
-	B.add(21);
-	System.out.println(A);
-	System.out.println(B);
-	System.out.println(c.merge(A,B));
+    
+    public ArrayList<Integer> merge(ArrayList<Integer> left, ArrayList<Integer> right) {
+	    ArrayList<Integer> mergedList = new ArrayList<Integer>();
+	    while(left.size() > 0 || right.size() > 0) {
+		if(left.size() > 0 && right.size() > 0) {
+		    if(left.get(0) < right.get(0)) {
+			mergedList.add(left.get(0));
+			left.remove(0);
+		    } else {
+			mergedList.add(right.get(0));
+			right.remove(0);
+		    }
+		} else if(left.size() > 0) {
+		    mergedList.add(left.get(0));
+		    left.remove(0);
+		} else if(right.size() > 0) {
+		    mergedList.add(right.get(0));
+		    right.remove(0);
+		}
+	    }
+	    return mergedList;
     }
     
+    public static void main(String[] args) {
+	ArrayList<Integer> A = new ArrayList<Integer>();
+	Random rn = new Random();
+	for (int i = 0; i < 10; i++) {
+	    A.add(rn.nextInt(15));
+	}
+        StuySort m = new Mergesort();
+	System.out.println("Array: " + A);
+	System.out.println(m.mergeSort(A));
+    }
+	
 }
