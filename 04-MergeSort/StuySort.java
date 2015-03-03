f9a13650b14bd75f8354d1c7a6efe809fde25b2f
Your Name <livelaughlovelol4@yahoo.com>
2015-03-03 08:27:37 -0500

    Fixed mergeSort

--- 04-MergeSort/StuySort.java
+++ 04-MergeSort/StuySort.java
@@ -7,12 +7,9 @@ public class StuySort {
 	if(n.size() <= 1) {
 	    return n;
 	}
-	ArrayList<Integer> result = new ArrayList<Integer>();
-	
 	ArrayList<Integer> left = new ArrayList<Integer>();
 	ArrayList<Integer> right = new ArrayList<Integer>();
 	int middle = n.size()/2;
-
 	for(int i = 0; i < n.size(); i++) {
 	    if(i < middle) {
 		left.add(n.get(i));
@@ -20,34 +17,34 @@ public class StuySort {
 		right.add(n.get(i));
 	    }
 	}
-	left = mergeSort(left); 
-	right = mergeSort(right);
-
-        result = merge(left, right);
-	return result;
+        return merge(mergeSort(left), mergeSort(right));
     }
     
     public ArrayList<Integer> merge(ArrayList<Integer> left, ArrayList<Integer> right) {
 	    ArrayList<Integer> mergedList = new ArrayList<Integer>();
-	    while(left.size() > 0 || right.size() > 0) {
-		if(left.size() > 0 && right.size() > 0) {
-		    if(left.get(0) < right.get(0)) {
-			mergedList.add(left.get(0));
-			left.remove(0);
-		    } else {
-			mergedList.add(right.get(0));
-			right.remove(0);
-		    }
-		} else if(left.size() > 0) {
+	    while(left.size() > 0 && right.size() > 0) {
+		if(left.get(0) < right.get(0)) {
 		    mergedList.add(left.get(0));
 		    left.remove(0);
-		} else if(right.size() > 0) {
+		} else {
 		    mergedList.add(right.get(0));
 		    right.remove(0);
 		}
 	    }
+	    if(left.size() > 0) {
+		while(left.size() > 0) {
+		    mergedList.add(left.get(0));
+		    left.remove(0);
+		    }
+	    } else if (right.size() > 0) {
+		while(right.size() > 0) {
+		    mergedList.add(right.get(0));
+		    right.remove(0);
+		    }
+	    }
 	    return mergedList;
     }
+
     
     public static void main(String[] args) {
 	ArrayList<Integer> A = new ArrayList<Integer>();
@@ -55,9 +52,10 @@ public class StuySort {
 	for (int i = 0; i < 10; i++) {
 	    A.add(rn.nextInt(15));
 	}
-        StuySort m = new Mergesort();
+        StuySort m = new StuySort();
 	System.out.println("Array: " + A);
-	System.out.println(m.mergeSort(A));
+	System.out.println("Sorted: " + m.mergeSort(A));
+	
     }
 	
 }
