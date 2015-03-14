8d3f071fa033e57e2a8f58e46eb7a8d869fff6b1
Caitlin Stanton <livelaughlovelol4@yahoo.com>
2015-03-13 21:05:14 -0400

    Update ListDriver.java

--- 07-LinkedList/ListDriver.java
+++ 07-LinkedList/ListDriver.java
@@ -1,3 +1,14 @@
-public ListDriver {
+import java.util.*;
 
+public class  Driver{
+		
+		public static void main(String[] args) {
+				Random r = new Random();
+				LList l = new LList();
+				for (int i=0; i < 10; i++){
+						l.add(""+i);
+				}
+				System.out.println(l);
+		}
+		
 }
