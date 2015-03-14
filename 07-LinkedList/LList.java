d88481e3c916a906d7e47e1b20c0ed21e2b810c6
Caitlin Stanton <livelaughlovelol4@yahoo.com>
2015-03-14 17:27:56 -0400

    DOOONE

--- 07-LinkedList/LList.java
+++ 07-LinkedList/LList.java
@@ -1,20 +1,51 @@
 public class LList {
-	private Node l=null;
+    
+    private Node l = null;
+    
+    public void add(String s){
+	Node tmp = new Node(s);
+	tmp.setNext(l);
+	l = tmp;
+    }
 
-	public void add(String s){
-		Node tmp = new Node(s);
-		tmp.setNext(l);
-		l = tmp;
+    public String find(int n) {
+	Node tmp = l;
+	int counter = -1;
+	String s = "";
+	for (tmp = l; tmp != null; tmp = tmp.getNext()) {
+	    counter = counter + 1;
+	    if (counter == n) {
+	        s = s + tmp;
+	    }
 	}
+	return s;
+    }
 
-	public String toString(){
-		String s = "";
-		Node tmp;
-		for (tmp = l; tmp != null; tmp=tmp.getNext()){
-			s = s + tmp + " --> ";
+    public void insert(int n, String s) {
+	Node added = new Node(s);
+	if (n == 0) {
+	    add(s);
+	} else {
+	    Node tmp = l;
+	    for (int i = 0; i < n - 1; tmp = tmp.getNext()) {
+		if (tmp == null) {
+		    throw new IndexOutOfBoundsException();
 		}
-		s = s + "null";
-		return s;
+		i++;
+	    }
+	    added.setNext(tmp.getNext());
+	    tmp.setNext(added);
 	}
-		
+    }
+    
+    public String toString(){
+	String s = "";
+	Node tmp;
+	for (tmp = l; tmp != null; tmp = tmp.getNext()){
+	    s = s + tmp + " --> ";
+	}
+	s = s + "null";
+	return s;
+    }
+    
 }
--- 07-LinkedList/ListDriver.java
+++ 07-LinkedList/ListDriver.java
@@ -1,14 +1,19 @@
 import java.util.*;
 
-public class  Driver{
-		
-		public static void main(String[] args) {
-				Random r = new Random();
-				LList l = new LList();
-				for (int i=0; i < 10; i++){
-						l.add(""+i);
-				}
-				System.out.println(l);
-		}
+public class ListDriver{
+    
+    public static void main(String[] args) {
+	Random r = new Random();
+	LList l = new LList();
+	for (int i = 0; i < 10; i++){
+	    l.add(""+i);
+	}
+	System.out.println(l);
+	System.out.println(l.find(4));
+	l.insert(4,"C");
+	System.out.println(l);
+	l.insert(0,"sup");
+	System.out.println(l);
+    }
 		
 }
