0f46cac7912d7bea6b0e78c5bf9ae318f0a3ab10
Your Name <livelaughlovelol4@yahoo.com>
2015-03-26 08:28:49 -0400

    DONE :D

---
+++ 11-Queue/Driver.java
@@ -0,0 +1,15 @@
+import java.lang.Integer;
+public class Driver {
+    public static void main(String[] args) {
+	myQueue<Integer> t = new myQueue<Integer>();
+	System.out.println(t.empty());
+	for (int i = 0 ; i < 10 ; i++) {
+	    t.enQueue(i);
+	}
+	System.out.println(t);
+	System.out.println(t.empty());
+	System.out.println(t.head());
+	System.out.println(t.deQueue());
+	System.out.println(t);
+    }
+}
