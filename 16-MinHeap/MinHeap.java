cbdff51322220c7ea140b5f8d4a6bb09cc7b1cae
Your Name <livelaughlovelol4@yahoo.com>
2015-05-12 08:33:10 -0400

    C

--- 16-MinHeap/MinHeap.java
+++ 16-MinHeap/MinHeap.java
@@ -3,6 +3,11 @@ public class MinHeap {
     //CHARACTERISTICS
          //Each child is greater than its parent
          //Each level is as full as possible from left to right
+
+    //RUNTIMES
+         //findMin --> O(1)
+         //removeMin, pushDown, insert --> O(log n)
+         //heapSort --> O(n * log n)
     
     /*
       public int removeMin(Node t) { }
@@ -12,6 +17,13 @@ public class MinHeap {
       3. Push down
           - Swap root with smaller child and continue done until
 	  the end of the tree is reached
+    */
+
+    /*
+      public void insert(Node t) { }
+      
+      1. Add new lowest left node
+      2. Sift up
     */
 
 }
