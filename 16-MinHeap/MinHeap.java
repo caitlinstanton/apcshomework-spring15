2d4e4b238d6711bd145162c87a5ba5d0d5ad9d51
Your Name <livelaughlovelol4@yahoo.com>
2015-05-12 08:27:57 -0400

    HEAPHEAPHEAP

---
+++ 16-MinHeap/MinHeap.java
@@ -0,0 +1,17 @@
+public class MinHeap {
+
+    //CHARACTERISTICS
+         //Each child is greater than its parent
+         //Each level is as full as possible from left to right
+    
+    /*
+      public int removeMin(Node t) { }
+
+      1. Remove min
+      2. Replace with lowest value to the right
+      3. Push down
+          - Swap root with smaller child and continue done until
+	  the end of the tree is reached
+    */
+
+}
